837596c2591ad9ae8183b2674ef24f1a215d8e56
leohe0528 <leohe0528@g.ucla.edu>
2019-08-27 13:49:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# StudyNoteApp
---
+++ StudyNote/KnowledgeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  KnowledgeDetailViewController.swift
+//  StudyNote
+//
+//  Created by Jian Feng He on 8/27/19.
+//  Copyright © 2019 Jian Feng He. All rights reserved.
+//
+
+import UIKit
+
+class KnowledgeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
